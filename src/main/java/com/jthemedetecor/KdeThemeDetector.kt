c92bc189d3445a4b74cae6b0cb6e9b8f06c4f68b
Zachary Wander <zachary.wander@gmail.com>
2025-12-24 20:41:27 -0500

    Fix build

--- src/main/java/com/jthemedetecor/KdeThemeDetector.kt
+++ src/main/java/com/jthemedetecor/KdeThemeDetector.kt
@@ -11,22 +11,12 @@
  *   See the License for the specific language governing permissions and
  *   limitations under the License.
  */
+package com.jthemedetecor
 
-package com.jthemedetecor;
-
-import com.jthemedetecor.util.ConcurrentHashSet;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Objects;
-import java.util.Set;
-import java.util.function.Consumer;
-import java.util.regex.Pattern;
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.util.regex.Pattern
 
 /**
  * Used for detecting the dark theme on a Linux KDE desktop environment.
@@ -35,89 +25,63 @@ import java.util.regex.Pattern;
  * @author Thomas Sartre
  * @see GnomeThemeDetector
  */
-public class KdeThemeDetector extends OsThemeDetector {
+class KdeThemeDetector : ThreadBasedOsThemeDetector() {
+    private val darkThemeNamePattern: Pattern =
+        Pattern.compile(".*dark.*", Pattern.CASE_INSENSITIVE)
 
-    private static final Logger logger = LoggerFactory.getLogger(KdeThemeDetector.class);
-
-    private static final String GET_THEME_CMD = "kreadconfig5 --file kdeglobals --group General --key ColorScheme";
-
-    private final Set<Consumer<Boolean>> listeners = new ConcurrentHashSet<>();
-    private final Pattern darkThemeNamePattern = Pattern.compile(".*dark.*", Pattern.CASE_INSENSITIVE);
-
-    private volatile DetectorThread detectorThread;
-
-    @Override
-    public boolean isDark() {
-        try {
-            Process process = Runtime.getRuntime().exec(GET_THEME_CMD);
-            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-                String theme = reader.readLine();
-                if (theme != null && isDarkTheme(theme)) {
-                    return true;
+    override val isDark: Boolean
+        get() {
+            try {
+                val process: Process =
+                    Runtime.getRuntime().exec(GET_THEME_CMD)
+                BufferedReader(InputStreamReader(process.inputStream)).use { reader ->
+                    val theme = reader.readLine()
+                    if (theme != null && isDarkTheme(theme)) {
+                        return true
+                    }
                 }
+            } catch (e: IOException) {
+                logger.error("Couldn't detect KDE OS theme", e)
             }
-        } catch (IOException e) {
-            logger.error("Couldn't detect KDE OS theme", e);
+            return false
         }
-        return false;
+
+    override fun createThread(): ThreadBasedOsThemeDetector.DetectorThread {
+        return DetectorThread(this)
     }
 
-    private boolean isDarkTheme(String theme) {
-        return darkThemeNamePattern.matcher(theme).matches();
-    }
-
-    @Override
-    public synchronized void registerListener(@NotNull Consumer<Boolean> darkThemeListener) {
-        Objects.requireNonNull(darkThemeListener);
-        boolean listenerAdded = listeners.add(darkThemeListener);
-        boolean singleListener = listenerAdded && listeners.size() == 1;
-
-        if (singleListener || (detectorThread != null && detectorThread.isInterrupted())) {
-            detectorThread = new DetectorThread(this);
-            detectorThread.start();
-        }
-    }
-
-    @Override
-    public synchronized void removeListener(@Nullable Consumer<Boolean> darkThemeListener) {
-        listeners.remove(darkThemeListener);
-        if (listeners.isEmpty() && detectorThread != null) {
-            detectorThread.interrupt();
-            detectorThread = null;
-        }
+    private fun isDarkTheme(theme: String): Boolean {
+        return darkThemeNamePattern.matcher(theme).matches()
     }
 
     /**
      * Thread implementation for detecting the actually changed theme.
      */
-    private static final class DetectorThread extends Thread {
-
-        private final KdeThemeDetector detector;
-        private boolean lastValue;
-
-        DetectorThread(@NotNull KdeThemeDetector detector) {
-            this.detector = detector;
-            this.lastValue = detector.isDark();
-            this.setName("KDE Theme Detector Thread");
-            this.setDaemon(true);
-            this.setPriority(Thread.NORM_PRIORITY - 1);
-        }
-
-        @Override
-        public void run() {
-            while (!this.isInterrupted()) {
-                boolean currentDetection = detector.isDark();
+    private inner class DetectorThread(private val detector: KdeThemeDetector) :
+        ThreadBasedOsThemeDetector.DetectorThread(
+            threadName = "KDE Theme Detector Thread",
+            daemon = true,
+            priority = NORM_PRIORITY - 1,
+        ) {
+        override fun run() {
+            while (!this.isInterrupted) {
+                val currentDetection = detector.isDark
                 if (currentDetection != lastValue) {
-                    lastValue = currentDetection;
-                    for (Consumer<Boolean> listener : detector.listeners) {
+                    lastValue = currentDetection
+                    for (listener in detector.listeners) {
                         try {
-                            listener.accept(currentDetection);
-                        } catch (RuntimeException e) {
-                            logger.error("Caught exception during listener notification", e);
+                            listener.accept(currentDetection)
+                        } catch (e: RuntimeException) {
+                            logger.error("Caught exception during listener notification", e)
                         }
                     }
                 }
             }
         }
+    }
+
+    companion object {
+        private const val GET_THEME_CMD =
+            "kreadconfig5 --file kdeglobals --group General --key ColorScheme"
     }
 }
